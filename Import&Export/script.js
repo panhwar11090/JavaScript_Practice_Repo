3083747cc9159674bce5569cd40536c319dad5ff
panhwar11090 <11090@kiet.edu.pk>
2023-08-07 13:03:34 +0500

    Import and Export

---
+++ Import&Export/script.js
@@ -0,0 +1,7 @@
+// import { addtion } from "./utility.js";
+import { addtion as add , subtraction as sub } from "./utility.js";
+// import * as utility from "./utility.js";
+
+
+console.log(add(4,5,1));
+console.log(sub(5,1));
---
+++ Import&Export/utility.js
@@ -0,0 +1,17 @@
+function addtion(a,b,c){
+    return a+b+c;
+}
+
+
+export default function multiplication(a,b){
+    return a*b;
+}
+
+function subtraction(a,b){
+    return a-b;
+}
+
+export{
+    addtion ,
+    subtraction ,
+}
