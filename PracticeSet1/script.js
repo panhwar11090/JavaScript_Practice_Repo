f9d6c0a22311a06f66e834c13776612deed40b32
panhwar11090 <11090@kiet.edu.pk>
2023-08-08 20:44:16 +0500

    practice set 1 completde

---
+++ PracticeSet1/script.js
@@ -0,0 +1,31 @@
+//Q1
+let a = "huzaifa";
+let b = 6;
+
+console.log(a+b);
+
+//Q3
+console.log(typeof(a+b));
+
+//Q3
+
+// const c= {
+//     name:"huzaifa",
+//     id: 11090,
+//     isPrinciple : false,
+// }
+
+// c ['name'] = "Ahsan";
+// c ['id'] = 11513
+
+
+
+// console.log(c)
+
+const dict  = {
+    name : "nam",
+    car: "gari"
+
+}
+
+console.log(dict.car);
