da19ff2f6d95b43839334436ff060f4084c33ce4
panhwar11090 <11090@kiet.edu.pk>
2023-08-10 21:51:35 +0500

    String Pratcice

---
+++ Strings/script.js
@@ -0,0 +1,70 @@
+
+let name= "Huzaifa";
+let partner = "AHSAN";
+console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name.toUpperCase());
+console.log(partner.toLowerCase());
+console.log(name.slice(2,6));
+console.log(name.slice(5));
+console.log(name.replace("H","O"))
+
+console.log(name.concat("is a friend of " + partner));
+let gap ="    Ayesha     ";
+console.log(gap.trim());
+console.log(gap);
+
+let fr = "Raman" + "Shivika" + "Harry"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr[4])
+console.log(fr[5])
+console.log(fr[6])
+
+
+for(let i=0;i<fr.length;i++){
+    console.log(fr);
+}
+
+
+
+let freind = 'prakash';
+
+
+//template letrals
+
+let boy1 = "pramod";
+let boy2 = "nikhil";
+// Nikhil is a friend of pramod
+let setnce =`${boy1} is a freind of ${boy2}`;
+console.log(setnce);
+
+
+
+// escape Sequance Character
+let fruit1 = 'Bana\'na'
+console.log(fruit1);
+let fruit = "Bana\"na";
+console.log(fruit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
