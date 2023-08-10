89c01a9545cad37a6dbabdf5fb0ea6b721d48cf3
panhwar11090 <11090@kiet.edu.pk>
2023-08-11 03:41:32 +0500

    chapter set 4 complete

---
+++ Pratcice set4/script.js
@@ -0,0 +1,18 @@
+//q1
+console.log("har\"".length);
+
+//q3
+
+let name = "HUZAIFA";
+console.log(name.toLowerCase());
+
+console.log(name.replace("I", "A"));
+
+let sent = `${name} is my freind`;
+console.log(sent.includes("hello"));
+
+
+let str1 = "plzz give Rs 1000";
+let amount = Number.parseInt(str1.slice("plzz give Rs ".length));
+console.log(typeof amount);
+console.log(amount);
