150666932d256fdeea3f273beb83324991692ba5
panhwar11090 <11090@kiet.edu.pk>
2023-08-14 04:35:49 +0500

    console objects

---
+++ console objects/script.js
@@ -0,0 +1,27 @@
+
+console.log("log");
+console.info("info");
+console.warn("warn");
+console.error("err");
+console.assert("err" !=false);
+console.assert("err" ==true);
+
+
+console.time("for loop");
+
+for(let i=0;i<5;i++){
+    console.log(233)
+}
+
+console.timeEnd("for loop");
+
+
+
+console.time("while loop");
+let a;
+while(a<5){
+    console.log(233)
+    a++;
+}
+
+console.timeEnd("while loop")
