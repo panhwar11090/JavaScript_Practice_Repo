500551b20f161059b4e4d32c59db150edf7a5ef0
panhwar11090 <11090@kiet.edu.pk>
2023-08-16 02:45:09 +0500

    element navigation

---
+++ Elemnt only navigation/script.js
@@ -0,0 +1,7 @@
+const changeBgRed = () => {
+    document.body.firstElementChild.style.background = "red"
+  }
+
+let b = document.body;
+console.log("first child of b is" , b.firstChild);
+console.log("first element chlid of b is " , b.firstElementChild);
