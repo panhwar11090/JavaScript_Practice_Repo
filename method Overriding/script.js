ee87631ff693bfdaf53647b25d3f6349e31a0a9a
panhwar11090 <11090@kiet.edu.pk>
2023-09-21 03:46:20 +0500

    method overriding

---
+++ method Overriding/script.js
@@ -0,0 +1,36 @@
+class Employee{
+    constructor(name){
+        console.log(`${name} - Employes constructor is here`);
+        this.name = name;
+    }
+    login(){
+        console.log("employee has loged in");
+    }
+
+    logout(){
+        console.log("employee has loged out");
+    }
+
+    requestLeaves(leaves){
+        console.log(`employe has requested ${leaves} leaves -auto aproved`);
+    }
+}
+
+class Programmer extends Employee{
+    constructor(name){
+        super(name)
+        console.log("this is newly written constructor");
+    }
+    requestCoffee(x){
+        console.log(`Employe has request ${x} coffees`);
+    }
+    requestLeaves(leaves){
+        super.requestLeaves(4);
+        console.log("one extra is granted");
+    }
+}
+
+let e = new Programmer("Harry");
+e.login()
+e.requestCoffee(3)
+e.requestLeaves(3)
