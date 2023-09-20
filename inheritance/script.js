251193ac3d14d23c5a6824bf47ef2d5a11047836
panhwar11090 <11090@kiet.edu.pk>
2023-09-21 02:41:23 +0500

    inheritance

---
+++ inheritance/script.js
@@ -0,0 +1,29 @@
+class Animal{
+    constructor(name , color){
+        this.name = name;
+        this.color = color;
+    }
+    run(){
+        console.log(this.name + " is running");
+    }
+    shout(){
+        console.log(this.name + " is shouting");
+    }
+}
+
+class Monkey extends Animal{
+    eatBanana(){
+        console.log(this.name + " is eating banana");
+    }
+    hide(){
+        console.log(this.name + " is hiding");
+    }
+}
+
+let ani = new Animal("Bruno", "White");
+let m = new Monkey("Chimpu", "orange");
+
+
+ani.shout();
+m.eatBanana();
+m.hide();
