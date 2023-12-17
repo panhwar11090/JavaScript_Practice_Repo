0a41e2706a0ef3ccf832232cefdec005cd1af21c
panhwar11090 <11090@kiet.edu.pk>
2023-12-18 03:02:22 +0500

    array part1

---
+++ PanacloudPractice/test.js
@@ -0,0 +1,30 @@
+// console.log("huzaifa");
+
+// const arr1 = [0,1,2,3,4,"huzaifa"];
+
+// console.log(arr1)
+
+// arr1.push(6);
+// arr1.push(7)
+// arr1.pop()
+// // console.log(arr1)
+
+// arr1.unshift("ali");
+// arr1.shift();
+// console.log(arr1)
+
+// const arr2 = arr1.join()
+// console.log(arr1)
+// console.log(arr2)
+
+
+
+
+// slice and splice
+
+const ar1 = [1,2,3,4,5,6,"ali","asfand"];
+console.log("a" , ar1);
+console.log("do slice" , ar1.slice(0,3))
+
+console.log("do splice", ar1.splice(0,3))
+console.log(ar1)
